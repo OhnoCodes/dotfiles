116026a927e18e5620568b279c38419a47e0702e
OhnoCodes <129247067+OhnoCodes@users.noreply.github.com>
2025-07-21 20:24:29 -0600

    Test: update theme colors

--- .p10k.zsh
+++ .p10k.zsh
@@ -362,6 +362,7 @@
   #####################################[ vcs: git status ]######################################
   # Version control background colors.
   typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND='#4B004B'
+  typeset -g POWERLEVEL9K_VCS_CLEAN_FOREGROUND='#50C878'
   typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND='#CC5500'
   typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND='#8E4585'
   typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND='#CC5500'
